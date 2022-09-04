8fe83076371783bb001f0988a10491d737c107b6
Julian <julian.koehn1989@googlemail.com>
2022-09-04 09:09:41 +0200

    basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+console.clear();
+
+
+
+
+
+
+export default function App() {
+
+const [item, setItem] = useState([]);
+
+
+
+
+
+  useEffect(() => {
+  
+    loadItem(); }, []);
+  
+  async function loadItem() {
+    try {
+      const response = await  fetch("https://fetch-me.vercel.app/api/shopping/items");
+      const data = await response.json();
+      setItem(data.shoppingitem);
+      console.log(data.shopping.item);
+  
+    }
+  }
+
+
+  
+  
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="header">SHOPPING LIST</div>
+      <div className="main">
+        <input placeholder="Was brauchst du?"></input>
+      </div>
+      <div className="footer">
+        <svg
+          className="footer-shoppingcart"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
         >
-          Learn React
-        </a>
-      </header>
+          <path d="M17,18A2,2 0 0,1 19,20A2,2 0 0,1 17,22C15.89,22 15,21.1 15,20C15,18.89 15.89,18 17,18M1,2H4.27L5.21,4H20A1,1 0 0,1 21,5C21,5.17 20.95,5.34 20.88,5.5L17.3,11.97C16.96,12.58 16.3,13 15.55,13H8.1L7.2,14.63L7.17,14.75A0.25,0.25 0 0,0 7.42,15H19V17H7C5.89,17 5,16.1 5,15C5,14.65 5.09,14.32 5.24,14.04L6.6,11.59L3,4H1V2M7,18A2,2 0 0,1 9,20A2,2 0 0,1 7,22C5.89,22 5,21.1 5,20C5,18.89 5.89,18 7,18M16,11L18.78,6H6.14L8.5,11H16Z" />
+        </svg>
+      </div>
     </div>
   );
 }
-
-export default App;
